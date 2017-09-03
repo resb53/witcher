96551620f37dd4e1a00ebf55781f3cbbca257cfb
resb53 <resb53.git@gmail.com>
2017-09-03 20:07:29 +0000

    Serve static files too

--- maze/web/maze-flask.py
+++ maze/web/maze-flask.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
   return render_template('./index.html')
 
+@app.route("/js/<path:path>")
+def send_js(path):
+  return send_from_directory('js', path)
+
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=int(8080))
+  app.run(host="0.0.0.0", port=int(80))
