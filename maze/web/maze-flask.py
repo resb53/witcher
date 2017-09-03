1f389b52ff512a946512b714e9ab3d678b393506
resb53 <resb53.git@gmail.com>
2017-09-03 20:20:37 +0000

    Added initial images

--- maze/web/maze-flask.py
+++ maze/web/maze-flask.py
@@ -12,5 +12,13 @@ def index():
 def send_js(path):
   return send_from_directory('js', path)
 
+@app.route("/css/<path:path>")
+def send_css(path):
+  return send_from_directory('css', path)
+
+@app.route("/img/<path:path>")
+def send_img(path):
+  return send_from_directory('img', path)
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=int(80))
