d63a82eeb95d620a93fb99627308648feda34e50
resb53 <resb53.git@gmail.com>
2017-09-23 16:50:30 +0000

    Correct image loads for given tile

--- maze/web/js/maze.js
+++ maze/web/js/maze.js
@@ -1,4 +1,4 @@
 // Code for navigating the maze and interacting with the website
 $(document).ready(function() {
-  $("#view").attr("src","gettile.jpg")
+  $("#view").attr("src","img/" + loc["zone"] + "/" + loc["image"])
 });
--- maze/web/maze-flask.py
+++ maze/web/maze-flask.py
@@ -2,27 +2,113 @@
 
 from flask import Flask, render_template, send_from_directory, request
 
+import sys
+import json
+
+# Prepare maze details
+with open('maze.json', 'r') as f:
+  maze = json.load(f)
+
+# Cardinal views
+cardinals = ["n", "e", "s", "w"]
+
+# base30
+axes = ["a","b","c","d","e","f","g","h","i","j",
+        "k","l","m","n","o","p","q","r","s","t",
+        "u","v","w","x","y","z","1","2","3","4"]
+
+# Valid tiles
+tiles = { "x" :"crossroads.jpg",
+          "st":"straight-t.jpg",
+          "lt":"left-t.jpg",
+          "rt":"right-t.jpg",
+          "s" :"straight.jpg",
+          "lb":"left-b.jpg",
+          "rb":"right-b.jpg",
+          "oc":"orb-cyan.jpg",
+          "or":"orb-red.jpg",
+          "op":"orb-purple.jpg",
+          "de":"dead-end.jpg" }
+
+# Options for each tile
+options = { "x" : ["w","d","s","a"],
+            "st": ["d","s","a" ],
+            "lt": ["w","s","a"],
+            "rt": ["w","d","s"],
+            "s" : ["w","s"],
+            "lb": ["s","a"],
+            "rb": ["d","s"],
+            "oc": ["s"],
+            "or": ["s"],
+            "op": ["s"],
+            "de": ["s"] }
+
+movement = ["w","d","s","a"]
+
+# Initialise
+curDir = 0
+curPos = "3k"
+
+# Parse tile id
+def setLoc(t):
+  parts = list(t)
+  global curDir
+  curDir = cardinals.index(parts[2])
+  global curPos
+  curPos = ''.join([ parts[0], parts[1] ]) 
+
+# Get colour from current grid position
+def getColour(pos):
+  [y, x] = breakdownLoc(pos)
+
+  if 0 <= y <= 9:
+    if 0 <= x <= 9:
+      col = "a"
+    elif 10 <= x <= 19:
+      col = "b"
+    elif 20 <= x <= 29:
+      col = "c"
+  elif 10 <= y <= 19:
+    if 0 <= x <= 9:
+      col = "d"
+    elif 10 <= x <= 19:
+      col = "e"
+    elif 20 <= x <= 29:
+      col = "f"
+  elif 20 <= y <= 29:
+    if 0 <= x <= 9:
+      col = "g"
+    elif 10 <= x <= 19:
+      col = "h"
+    elif 20 <= x <= 29:
+      col = "i"
+
+  return col
+
+#Separate location into numeric values for each axes
+def breakdownLoc(pos):
+  yi = axes.index(pos[0])
+  xi = axes.index(pos[1])
+  return [yi, xi]
+
+# Create webapp
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  # If new player, give starting tile
-  cur_tile = "n3kh"
   args = request.args
-
-  # Parse and check the tile request
+  # Parse and check any specific tile request
   if "t" in args:
-    cur_tile = args["t"]
-  loc = parse_tile(cur_tile)
+    setLoc(args["t"].lower())
+  else:
+    setLoc("3kn")
+
+  loc = { "face": cardinals[curDir],
+          "tile": curPos,
+          "zone": getColour(curPos),
+          "image": tiles[maze[cardinals[curDir]][curPos]] }
 
   return render_template('./index.html', loc=loc)
-
-@app.route("/gettile.jpg")
-def get_tile():
-  # Calculate appropriate tile image and return it
-  image = "h/straight.jpg"
-
-  return send_from_directory('img', image)
 
 @app.route("/js/<path:path>")
 def send_js(path):
@@ -36,12 +122,12 @@ def send_css(path):
 def send_img(path):
   return send_from_directory('img', path)
 
-def parse_tile(t):
-  parts = list(t)
-  loc = { "face": parts[0],
-          "tile": ''.join([parts[1],parts[2]]),
-          "zone": parts[3] }
-  return loc
+# Maze specific web features
+#@app.route("/gettile.jpg")
+#def get_tile():
+  # Calculate appropriate tile image and return it
+#  image = "/".join([ getColour(curPos), tiles[maze[cardinals[curDir]][curPos]] ])
+#  return send_from_directory('img', image)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=int(80))
