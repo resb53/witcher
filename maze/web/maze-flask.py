4057124ad29d280fc852047f3cbbee70bc183112
resb53 <resb53.git@gmail.com>
2017-09-23 14:44:00 +0000

    Test with Flask variables for location parsing

--- maze/web/maze-flask.py
+++ maze/web/maze-flask.py
@@ -4,9 +4,14 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", method=['GET'])
 def index():
-  return render_template('./index.html')
+  cur_tile = "n3kh"
+  args = request.args
+  if "t" in args:
+    cur_tile = args.t
+  loc = parse_tile(cur_tile)
+  return render_template('./index.html', loc=loc)
 
 @app.route("/js/<path:path>")
 def send_js(path):
@@ -19,6 +24,13 @@ def send_css(path):
 @app.route("/img/<path:path>")
 def send_img(path):
   return send_from_directory('img', path)
+
+def parse_tile(t):
+  parts = list(t)
+  loc = { "face": parts[0],
+          "tile": ''.join([parts[1],parts[2]),
+          "zone": parts[3] }
+  return loc
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=int(80))
