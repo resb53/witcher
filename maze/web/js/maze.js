4e554311b2b00dcf72cef9d3ec26ef4faac9a526
resb53 <resb53.git@gmail.com>
2017-09-23 16:57:24 +0000

    Track position via image request parameters

--- maze/web/js/maze.js
+++ maze/web/js/maze.js
@@ -1,4 +1,4 @@
 // Code for navigating the maze and interacting with the website
 $(document).ready(function() {
-  $("#view").attr("src","img/" + loc["zone"] + "/" + loc["image"])
+  $("#view").attr("src","img/" + loc["zone"] + "/" + loc["image"] + "?t=" + loc["tile"] + loc["face"])
 });
