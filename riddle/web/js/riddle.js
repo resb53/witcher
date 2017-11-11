d7d264d31db2e84ce0057d1c6d9d934c83fc55b0
resb53 <resb53.git@gmail.com>
2017-11-11 14:39:31 +0000

    Basic input and response display funtionality

--- riddle/web/js/riddle.js
+++ riddle/web/js/riddle.js
@@ -1,4 +1,17 @@
 // Code for initialising the view
 $(document).ready(function() {
   $("#view").attr("src","img/init.jpg");
+  // Prepare action when text submitted
+  $("#query").on("submit", function(e) {
+    e.preventDefault();
+    // Loop through output lines, updating them
+    new_text = '> ' + $("#solution").val();
+    $("#solution").val('');
+    old_text = '';
+    for (i=0; i<10; i++) {
+      old_text = $("#record_"+i).text();
+      $("#record_"+i).text(new_text);
+      new_text = old_text;
+    }
+  });
 });
