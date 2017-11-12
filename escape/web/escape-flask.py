190268bc25d21191bacae11982498ac9daf071a5
resb53 <resb53.git@gmail.com>
2017-11-12 17:36:38 +0000

    More verbose help text for basic commands

--- escape/web/escape-flask.py
+++ escape/web/escape-flask.py
@@ -5,6 +5,31 @@ from flask import Flask, render_template, send_from_directory, request
 import sys
 import json
 
+# App logic
+commands = { '?': 'Shows the help text for a command.',
+             'look': 'Looks around the area, listing objects of interest.',
+             'use': 'Uses or interacts with an object that can be seen.',
+             'back': 'Backs away form an object to take a wider look.' }
+
+# Execute a valid command (checked in process_query)
+def doCommand(com):
+  ret = { 'msg': 'default response' }
+  # Process per command
+  # List available commands
+  if com[0] == '?':
+    if len(com) == 1:
+      ret['msg'] = ' &emsp; '.join(commands.keys())
+    elif len(com) == 2:
+      if com[1] in commands:
+        ret['msg'] = com[1] + ': ' + commands[com[1]]
+    else:
+      ret['msg'] = 'Too many arguments. ? can only be used with up to one additional command.'
+  elif com[0] == 'look':
+    ret['msg'] = 'You look around.'
+  elif com[0] == 'use':
+    ret['msg'] = 'You use an object.'
+  return ret
+
 # Create webapp
 app = Flask(__name__)
 
@@ -17,7 +42,12 @@ def process_query():
   data = request.form
   print(data, file=sys.stderr)
   if 'command' in data:
-    response = { 'msg' : data['command'] + " received." }
+    command = data['command'].lower()
+    comlist = command.split(' ')
+    if comlist[0] in commands:
+      response = doCommand(comlist)
+    else:
+      response = { 'msg' : 'Error: command not recognised. Use ? to list available commands, and ? followed by a command for more info.' }
   else:
     response = { 'msg' : "There seems to have been a problem. Please inform the DM." }
   return json.dumps(response)
--- escape/web/js/escape.js
+++ escape/web/js/escape.js
@@ -44,8 +44,8 @@ function update_log(newval) {
   new_text = newval;
   old_text = '';
   for (i=0; i<20; i++) {
-    old_text = $("#record_"+i).text();
-    $("#record_"+i).text(new_text);
+    old_text = $("#record_"+i).html();
+    $("#record_"+i).html(new_text);
     new_text = old_text;
   }
 }
